585a09b314425886fd95da1befdfda40dcee0b49
billy <kimocibill@gmail.com>
2024-07-09 21:04:59 -0700

    menambahkan kotak1 dan kotak2

---
+++ frontend/src/components/Kotak1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Kotak1() {
+  return (
+    <div>Kotak1</div>
+  )
+}
+
+export default Kotak1
---
+++ frontend/src/components/Kotak2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Kotak2() {
+  return (
+    <div>Kotak2</div>
+  )
+}
+
+export default Kotak2
